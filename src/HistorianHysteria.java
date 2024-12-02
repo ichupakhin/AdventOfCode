cf391a944575326b60466bbc0f3bff6b890c907c
EXXETA-DE\ilch <ilia.chupakhin@exxeta.com>
2024-12-02 13:24:38 +0100

    solved first day challenge

---
+++ src/HistorianHysteria.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HistorianHysteria {
+
+
+    public static int computeDistance(String filePath) {
+      int distance = 0;
+      ArrayList<Integer> firstList = new ArrayList<>();
+      ArrayList<Integer> secondList = new ArrayList<>();
+      try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+          // Process the line
+          String[] locationIds = line.split("\\s+");
+          firstList.add(Integer.parseInt(locationIds[0]));
+          secondList.add(Integer.parseInt(locationIds[1]));
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        return -1;
+      }
+      firstList.sort(Integer::compareTo);
+      secondList.sort(Integer::compareTo);
+
+      for(int i = 0; i < firstList.size(); i++) {
+        distance += Math.abs(firstList.get(i) - secondList.get(i));
+      }
+
+      return distance;
+    }
+
+  public static int computeDistanceStream(String filePath) {
+    ArrayList<Integer> firstList = new ArrayList<>();
+    ArrayList<Integer> secondList = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+      // Read the file and save location ids in two lists
+      br.lines()
+          .map(line -> line.split("\\s+"))
+          .forEach(locationIds -> {
+            firstList.add(Integer.parseInt(locationIds[0]));
+            secondList.add(Integer.parseInt(locationIds[1]));
+          });
+      // Sort the lists
+      firstList.sort(Integer::compareTo);
+      secondList.sort(Integer::compareTo);
+      // Compute the distance
+      return firstList.stream()
+          .mapToInt(firstListValue -> Math.abs(firstListValue - secondList.get(firstList.indexOf(firstListValue))))
+          .sum();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return -1;
+    }
+  }
+
+  public static long computeNumberOfSafeLevels(String filePath) {
+    try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+      return br.lines()
+          .map(line -> line.split("\\s+"))
+          .filter(HistorianHysteria::isSafeLevel)
+          .count();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return -1;
+    }
+  }
+
+  private static boolean isSafeLevel(String[] level) {
+    int lastDiff = 0;
+    for (int i = 1; i < level.length; i++) {
+      int currentDiff = Integer.parseInt(level[i]) -  Integer.parseInt(level[i-1]);
+      //1 till 3
+      if(Math.abs(currentDiff) > 3 || Math.abs(currentDiff) < 1) {
+        return false;
+      }
+
+      if(lastDiff != 0) { //0 means no previous diff
+        //The complete level must be either increasing or decreasing.
+        if(lastDiff > 0 && currentDiff < 0
+            || lastDiff < 0 && currentDiff > 0) {
+          return false;
+        }
+      }
+
+      lastDiff = currentDiff;
+    }
+    return true;
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("HistorianHysteria: computed distance is " + HistorianHysteria.computeDistance(
+        "resources/puzzle_input_day_1_task_1"));
+    System.out.println("HistorianHysteria: computed distance is " + HistorianHysteria.computeDistanceStream("resources/puzzle_input_day_1_task_1"));
+    System.out.println("HistorianHysteria: computed number of safe levels is " + HistorianHysteria.computeNumberOfSafeLevels("resources/puzzle_input_day_1_task_2"));
+  }
+}
